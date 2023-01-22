99a0dbc949314d2070538c49bf4f361c9c56dde2
Tohirjon <82634626+Tohirjon-Odilov@users.noreply.github.com>
2023-01-22 19:12:24 +0500

    tablet

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel();
 });
+
+let menu = document.querySelector(".menu-bar");
+let close = document.querySelector(".close-bar");
+let headerList = document.querySelector(".header__list");
+
+menu.addEventListener("click", () => {
+  close.classList.toggle("show");
+  menu.classList.toggle("hide");
+  headerList.classList.add("top");
+  headerList.classList.remove("bottom");
+});
+
+close.addEventListener("click", () => {
+  close.classList.toggle("show");
+  menu.classList.toggle("hide");
+  headerList.classList.remove("top");
+  headerList.classList.add("bottom");
+});
